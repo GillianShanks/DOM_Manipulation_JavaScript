1bd106ae4e0a88d9238c3e89000ee4155a7cb112
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-24 12:22:18 +0100

    edit addNewElement function to take in any parentElement so that an extra div can be created inside the element (for size so that it can be given flex-direction: row). Create size div but is not populated unless the variables for size are given.

--- js/app.js
+++ js/app.js
@@ -7,21 +7,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
 })
 
-const addNewElement = (appendToID, type, idName, itemID, text) => {
-  const parentElement = document.querySelector(`#${appendToID}`);
-  var childElement = document.createElement(`${type}`);
+const addNewElement = (parentElement, type, idName) => {
+  const childElement = document.createElement(`${type}`);
   childElement.id = `${idName}`;
   parentElement.appendChild(childElement);
   return childElement;
 };
 
-const addElementItem = (appendTo, type, idName, itemID, text) => {
-  const childElement = document.createElement(`${type}`);
-  if (itemID){
+const addElementItem = (appendTo, type, idName, className, itemID, text) => {
+  if (event.target[itemID].value) {
+    const childElement = document.createElement(`${type}`);
+    childElement.classList.add(`${className}`);
+    childElement.id = `${idName}`;
     childElement.textContent = `${text}` + event.target[itemID].value;
+    appendTo.appendChild(childElement);
+    return childElement;
   }
-  childElement.id = `${idName}`;
-  appendTo.appendChild(childElement);
 };
 
 
@@ -29,14 +30,22 @@ const handleSubmit = (event) => {
   event.preventDefault();
   // console.log(event.target.height.value);
 
-  const height_val =event.target.height.value;
-  const width_val =event.target.width.value;
+  const art_list = document.querySelector('#art-list');
 
-  const item = addNewElement('art-list', 'div', 'list-item');
-  addElementItem(item, 'h2', 'item-line', 'desc', 'Description: ');
-  addElementItem(item, 'h3', 'item-line', 'type', 'Type: ');
+  const item = addNewElement(art_list, 'div', 'list-item');
+  addElementItem(item, 'h2', 'item-line', 'item-desc', 'desc', 'Description: ');
+  addElementItem(item, 'h3', 'item-line', 'item-type', 'type', 'Type: ');
+
+  const size = addNewElement(item, 'div', 'item-size');
+  const height = addElementItem(size, 'p', 'item-line', 'item-height', 'height', 'Size (cm): ');
+  const width = addElementItem(size, 'p', 'item-line', 'item-width', 'width', " x ");
+
+  addElementItem(item, 'p', 'item-line', 'item-path', 'path', 'Path: ');
 
 
+  event.target.reset();
+
+//UN-REFACTORED CODE (for reference)
   // const list = document.querySelector('#art-list');
   // const item = document.createElement('div');
   // item.id = 'list-item';
@@ -52,17 +61,19 @@ const handleSubmit = (event) => {
   // type.textContent = `Type: ${type_val}`;
   // item.appendChild(type);
 
-  if (height_val && width_val){
-  const size = document.createElement('p');
-  size.textContent = `Size (hxw): ${event.target.height.value}cm x ${event.target.width.value}cm`;
-  item.appendChild(size);
-  }
 
-  addElementItem(item, 'p', 'item-line', 'path', 'Path: ');
+  // const height_val =event.target.height.value;
+  // const width_val =event.target.width.value;
+
+  // if (height_val && width_val){
+  // const size = document.createElement('p');
+  // size.textContent = `Size (hxw): ${event.target.height.value}cm x ${event.target.width.value}cm`;
+  // item.appendChild(size);
+  // }
+
   // const path = document.createElement('p');
   // path.textContent = `Path: ${path_val}`;
   // item.appendChild(path);
 
-  event.target.reset();
 
 }
