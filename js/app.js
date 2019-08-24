155cf6976892c6c13e3596e5021f7641535501eb
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-24 11:36:02 +0100

    refactored to two helper functions for adding a new list element, and adding items in that list element. Working on optional height and width values

--- js/app.js
+++ js/app.js
@@ -7,40 +7,62 @@ document.addEventListener('DOMContentLoaded', () => {
 
 })
 
+const addNewElement = (appendToID, type, idName, itemID, text) => {
+  const parentElement = document.querySelector(`#${appendToID}`);
+  var childElement = document.createElement(`${type}`);
+  childElement.id = `${idName}`;
+  parentElement.appendChild(childElement);
+  return childElement;
+};
+
+const addElementItem = (appendTo, type, idName, itemID, text) => {
+  const childElement = document.createElement(`${type}`);
+  if (itemID){
+    childElement.textContent = `${text}` + event.target[itemID].value;
+  }
+  childElement.id = `${idName}`;
+  appendTo.appendChild(childElement);
+};
+
+
 const handleSubmit = (event) => {
   event.preventDefault();
-  console.log(event.target.height.value);
+  // console.log(event.target.height.value);
 
-  const desc_val = event.target.desc.value;
-  const type_val = event.target.type.value;
-  const height_val = event.target.height.value;
-  const width_val = event.target.width.value;
-  const path_val = event.target.path.value;
+  const height_val =event.target.height.value;
+  const width_val =event.target.width.value;
+
+  const item = addNewElement('art-list', 'div', 'list-item');
+  addElementItem(item, 'h2', 'item-line', 'desc', 'Description: ');
+  addElementItem(item, 'h3', 'item-line', 'type', 'Type: ');
 
 
-  const list = document.querySelector('#art-list');
-  const item = document.createElement('div');
-  item.classList.add('list_item');
-  list.appendChild(item);
+  // const list = document.querySelector('#art-list');
+  // const item = document.createElement('div');
+  // item.id = 'list-item';
+  // list.appendChild(item);
 
-  const desc = document.createElement('h2');
-  desc.textContent = `Description: ${desc_val}`;
-  item.appendChild(desc);
+  // const item = document.querySelector(`#list-item`);
+  // const desc = document.createElement('h2');
+  // item.appendChild(desc);
+  // desc.textContent = `Description: ${event.target.desc.value}`;
 
-  const type = document.createElement('h3');
-  type.textContent = `Type: ${type_val}`;
-  item.appendChild(type);
+
+  // const type = document.createElement('h3');
+  // type.textContent = `Type: ${type_val}`;
+  // item.appendChild(type);
 
   if (height_val && width_val){
   const size = document.createElement('p');
-  size.textContent = `Size (hxw): ${height_val}cm x ${width_val}cm`;
+  size.textContent = `Size (hxw): ${event.target.height.value}cm x ${event.target.width.value}cm`;
   item.appendChild(size);
   }
 
-  const path = document.createElement('p');
-  path.textContent = `Path: ${path_val}`;
-  item.appendChild(path);
+  addElementItem(item, 'p', 'item-line', 'path', 'Path: ');
+  // const path = document.createElement('p');
+  // path.textContent = `Path: ${path_val}`;
+  // item.appendChild(path);
 
-
+  event.target.reset();
 
 }
