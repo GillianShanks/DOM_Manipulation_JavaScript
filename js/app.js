cb6e1d6b06b6065e2ddfc8e66dac27f3cb1a4af2
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-24 18:26:39 +0100

    adds radio buttons to change the size of images once it is clicked by adding/removing classNames to the element. Currently all images are submitted at full-size

--- js/app.js
+++ js/app.js
@@ -7,13 +7,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const deleteAll = document.querySelector('#delete-all');
   deleteAll.addEventListener('click', handleDeleteClick);
 
-  // const size_form = document.querySelectorAll('')
+  const small_btn = document.querySelector('#small');
+  small_btn.addEventListener('click', handleSizeClick);
+
+  const large_btn = document.querySelector('#large');
+  large_btn.addEventListener('click', handleSizeClick);
+
 
 })
 
 
+
 //use two separate helper functions so that it does not at the item-line when there is no input from form (so there are two types of element that can be added: one without an event.target and one with an event.target)
-  // refactored to use one helper function and put the if statement in the addEventListener>callbackfunction
+// refactored to use one helper function and put the if statement in the addEventListener>callbackfunction
 
 const addElement = (parentElement, type, idName, classNames=[], text, formID) => {
   const childElement = document.createElement(`${type}`);
@@ -35,9 +41,8 @@ const addElement = (parentElement, type, idName, classNames=[], text, formID) =>
 //     childElement.textContent = `${text}` + event.target[formID].value;
 //   };
 // };
-const handleDeleteClick = (event) => {
-  const art_list = document.querySelector('#art-list');
-  art_list.innerHTML= "";
+const selectedSize = () => {
+  
 }
 
 const handleSubmit = (event) => {
@@ -51,19 +56,21 @@ const handleSubmit = (event) => {
   const img = addElement(item, 'img', 'item-img', ['item-line'], '');
   img.src = event.target.path.value;
   img.alt = event.target.desc.value; // to replace with new form element?
+  const selectedSize = selectedSize();
+
   addElement(item, 'h2', 'item-desc', ['item-line'], 'Description: ', 'desc');
   addElement(item, 'h3', 'item-type', ['item-line'], 'Type: ', 'type');
 
   if (event.target.height.value && event.target.width.value) {
     const size = addElement(item, 'div', 'item-size', ['item-line', 'flex', 'flex-row'], '');
-      addElement(size, 'p', 'item-height', ['item-line'], 'Size (cm): ', 'height');
-      addElement(size, 'p', 'item-width', ['item-line'], "x", 'width');
+    addElement(size, 'p', 'item-height', ['item-line'], 'Size (cm): ', 'height');
+    addElement(size, 'p', 'item-width', ['item-line'], "x", 'width');
   }
 
 
   event.target.reset();
 
-//UN-REFACTORED CODE (for reference)
+  //UN-REFACTORED CODE (for reference)
   // const list = document.querySelector('#art-list');
   // const item = document.createElement('div');
   // item.id = 'list-item';
@@ -84,7 +91,7 @@ const handleSubmit = (event) => {
   // const height_val =event.target.height.value;
   // const width_val =event.target.width.value;
 
-//add div around height and width
+  //add div around height and width
   // if (height_val && width_val){
   // const size = document.createElement('p');
   // size.textContent = `Size (hxw): ${event.target.height.value}cm x ${event.target.width.value}cm`;
@@ -97,3 +104,36 @@ const handleSubmit = (event) => {
 
 
 }
+
+
+const handleDeleteClick = () => {
+  const art_list = document.querySelector('#art-list');
+  art_list.innerHTML= "";
+};
+
+
+
+const handleSizeClick = (event) => {
+  // console.log(event.target.value);
+  const imgs = document.querySelectorAll('#item-img');
+
+  if (event.target.value === 'small'){
+    imgs.forEach((img) => {
+      if (img.classList.contains('large')){
+        img.classList.remove('large');
+      }
+      if (!img.classList.contains('small')){
+        img.classList.add('small');
+      }
+    });
+  } else if (event.target.value === 'large'){
+    imgs.forEach((img) => {
+      if (img.classList.contains('small')){
+        img.classList.remove('small');
+      }
+      if (!img.classList.contains('large')){
+        img.classList.add('large');
+      }
+    });
+  };
+};
